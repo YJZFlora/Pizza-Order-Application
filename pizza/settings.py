37e9c3853a2eef85fbdc1d5c57d1f08fa6157b01
YJZFlora <florayjz@gmail.com>
2020-05-20 14:34:24 -0500

    add

--- pizza/settings.py
+++ pizza/settings.py
@@ -117,9 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
 import django_heroku
