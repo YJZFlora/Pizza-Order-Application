ba0aee1f571b8e6253827e678bbc21b57e3753c5
YJZFlora <florayjz@gmail.com>
2020-05-21 16:25:25 -0500

    secretkey

--- pizza/settings.py
+++ pizza/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'douglas_db',
+        'USER': 'super',
+        'PASSWORD': '715924',
+        'HOST': 'https://florapizzahouse.herokuapp.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
