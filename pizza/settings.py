16a91390ef52065716e62b27eb174ada31afcd5f
YJZFlora <florayjz@gmail.com>
2020-05-22 14:38:19 -0500

    change tample dir

--- pizza/settings.py
+++ pizza/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'pizza.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'USER': 'nlmtnywwgovyas',
     }
 }
 
